f34c717631661a0606b4bf5e731bf447496fa1ce
Mohammed Falah <falahsss900@gmail.com>
2023-09-26 21:38:18 +0530

    table functionality added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import CardDisplay from "./components/Card/CardDisplay";
 import Form from "./components/Form/Form";
 import { useState } from "react";
+import BasicTable from "./components/Table/Table";
 
 function App() {
   const [data, setData] = useState([]);
@@ -10,6 +11,7 @@ function App() {
     <div>
       <Form fetchData={setData} />
       <CardDisplay data={data} />
+      <BasicTable data={data} />
     </div>
   );
 }
